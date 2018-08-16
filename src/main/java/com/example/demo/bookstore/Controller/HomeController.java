8d206baa0e4785febe8c8db02f29aea825121c12
Prakash <prakashdas1998.pd@gmail.com>
2018-08-17 00:20:56 +0530

    Added fix

---
+++ src/main/java/com/example/demo/bookstore/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.example.demo.bookstore.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by prakashdas on 16/08/18.
+ */
+
+@Controller
+public class HomeController {
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+}
