6914df2c40a79d6c918a2f26a0f4b4c638df3386
Prakash <prakashdas1998.pd@gmail.com>
2018-08-19 23:35:40 +0530

    Fixed bugs

---
+++ src/main/java/com/example/demo/repository/BookRepository.java
@@ -0,0 +1,7 @@
+package com.example.demo.repository;
+
+/**
+ * Created by prakashdas on 19/08/18.
+ */
+public class BookRepository {
+}
--- src/main/java/com/example/demo/service/impl/BookServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/BookServiceImpl.java
@@ -1,7 +1,9 @@
 package com.example.demo.service.impl;
 
+import com.example.demo.repository.BookRepository;
 import com.example.demo.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.awt.print.Book;
 import java.util.List;
@@ -9,11 +11,12 @@ import java.util.List;
 /**
  * Created by prakashdas on 19/08/18.
  */
+@Service
 public class BookServiceImpl implements BookService {
     @Autowired
     private BookRepository bookRepository;
 
     public List<Book> findAll(){
-        return bookRepository.findAll();
+        return (List<Book>) bookRepository.findAll();
     }
 }
