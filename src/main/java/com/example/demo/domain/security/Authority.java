1dfce2bbb70ef42e6d3bda336ac2f1218aae4cb6
Prakash <prakashdas1998.pd@gmail.com>
2018-09-17 13:05:31 +0530

    Refactored code

--- src/main/java/com/example/demo/domain/security/Authority.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.domain.security;
-
-/**
- * Created by prakashdas on 17/08/18.
- */
-public class Authority {
-    public Authority(String name) {
-    }
-}
--- src/main/java/com/example/demo/repository/BookRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.repository;
-
-/**
- * Created by prakashdas on 19/08/18.
- */
-public class BookRepository {
-}
